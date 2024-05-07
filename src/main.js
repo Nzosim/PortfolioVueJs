77919f12a1e29dc624be1699205b11b2c8239f38
nzosim <nzosim54@gmail.com>
2024-05-07 18:24:41 +0200

    Init project and add vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  })
+
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
