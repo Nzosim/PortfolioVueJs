a578d4b46a7ef6cba6d22a92a9397bca785add1b
nzosim <nzosim54@gmail.com>
2024-05-10 15:47:27 +0200

    add responsive

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="home">
     <div class="home_left">
-      <p class="text-white text-h3">SIMONIN Enzo</p>
-      <p class="text-red-lighten-2 text-h5">> Apprentice Fullstack Developper</p>
+      <p class="name text-white text-h3">SIMONIN Enzo</p>
+      <p class="job text-red-lighten-2 text-h5">> Apprentice Fullstack Developper</p>
     </div>
-    <!-- <img class="home_right" src="../assets/Homepage.png" /> -->
+    <img class="home_right" src="../assets/Homepage.png" />
   </div>
 </template>
 
@@ -31,5 +31,41 @@ p {
   top: 50%;
   left: 50%;
   transform: translateY(-50%);
+}
+
+@media screen and (max-width: 1600px) {
+  .home_left {
+    left: 10%;
+  }
+  .home_right {
+    height: 400px;
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  .home_left {
+    left: 5%;
+  }
+  .name {
+    font-size: 2.5rem;
+  }
+  .job {
+    font-size: 1.5rem;
+  }
+}
+
+@media screen and (max-width: 930px) {
+  .home_left {
+    top: 20%;
+    left: 20%;
+    width: 60%;
+  }
+
+  .home_right {
+    top: 65%;
+    left: 20%;
+    width: 60%;
+    height: auto;
+  }
 }
 </style>
