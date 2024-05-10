956a64a318a82400a44e1af3edcbea5d2992a40c
nzosim <nzosim54@gmail.com>
2024-05-10 16:06:32 +0200

    remove analytics

--- src/main.js
+++ src/main.js
@@ -12,9 +12,6 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '@mdi/font/css/materialdesignicons.css'
 
-import { SpeedInsights } from '@vercel/speed-insights/vue'
-import { Analytics } from '@vercel/analytics/react'
-
 const app = createApp(App)
 
 const vuetify = createVuetify({
@@ -25,7 +22,5 @@ const vuetify = createVuetify({
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
-app.use(SpeedInsights)
-app.use(Analytics)
 
 app.mount('#app')
