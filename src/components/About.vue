2344c9eced232d2f60d06ad3b2ace28e71a248c4
nzosim <nzosim54@gmail.com>
2024-05-23 00:19:20 +0200

    navbar

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <div class="menu">
-      <div class="d-flex menu-all">
+      <div class="d-flex menu-all pt-3 pb-2 pl-2">
         <v-icon>mdi-arrow-down-drop-circle</v-icon>
         <p>personal-info</p>
       </div>
@@ -10,7 +10,7 @@
           <v-icon>mdi-folder-open-outline</v-icon>
           <p>bio</p>
         </div>
-        <div class="d-flex">
+        <div class="d-flex text-white ml-4">
           <v-icon>mdi-information</v-icon>
           <p>about-me</p>
         </div>
@@ -18,31 +18,35 @@
           <v-icon>mdi-folder</v-icon>
           <p>experiences</p>
         </div>
-        <div class="d-flex">
+        <div class="d-flex ml-4">
           <v-icon>mdi-information</v-icon>
           <p>education</p>
         </div>
-        <div class="d-flex">
+        <div class="d-flex ml-4">
           <v-icon>mdi-information</v-icon>
           <p>internship</p>
         </div>
-        <div class="d-flex">
+        <div class="d-flex ml-4">
           <v-icon>mdi-information</v-icon>
           <p>apprentice</p>
         </div>
       </div>
       <div class="contact">
-        <div class="d-flex menu-all">
+        <div class="d-flex menu-all pt-3 pb-2 pl-2">
           <v-icon>mdi-arrow-down-drop-circle</v-icon>
           <p>contact</p>
         </div>
-        <div class="d-flex">
+        <div class="d-flex ml-4">
           <v-icon>mdi-information</v-icon>
           <p>contact</p>
         </div>
       </div>
     </div>
     <div class="page">
+      <div class="close">
+        <p>about-me</p>
+        <v-icon>mdi-close</v-icon>
+      </div>
       <div class="text">
         <p># SIMONIN Enzo</p>
         <p>
@@ -57,18 +61,25 @@
       </div>
     </div>
     <div class="preview">
-      <h1>SIMONIN Enzo</h1>
-      <p>
-        Je suis <span class="text-yellow">actuellement</span> en troisième année de
-        <b>BUT Informatique</b> en <span class="text-yellow">alternance</span> chez
-        <b>Dassault Systèmes</b> à Nancy.
-      </p>
-      <p>
-        Futur étudiant de <b>Polytech Nancy</b> en cycle ingénieur Informatique Automatisme
-        Robotique et Réseaux <span class="text-yellow">(IA2R)</span>, je suis à la recherche d'une
-        alternance pour les <span class="text-yellow">3</span> années à venir.
-      </p>
-      <p>Mon objectif est d'appronfindir mes connaissances grâce à la pratique en entreprise.</p>
+      <div class="close">
+        <v-icon>mdi-file-find</v-icon>
+        <p>preview</p>
+        <v-icon>mdi-close</v-icon>
+      </div>
+      <div class="text">
+        <h1>SIMONIN Enzo</h1>
+        <p>
+          Je suis <span class="text-yellow">actuellement</span> en troisième année de
+          <b>BUT Informatique</b> en <span class="text-yellow">alternance</span> chez
+          <b>Dassault Systèmes</b> à Nancy.
+        </p>
+        <p>
+          Futur étudiant de <b>Polytech Nancy</b> en cycle ingénieur Informatique Automatisme
+          Robotique et Réseaux <span class="text-yellow">(IA2R)</span>, je suis à la recherche d'une
+          alternance pour les <span class="text-yellow">3</span> années à venir.
+        </p>
+        <p>Mon objectif est d'appronfindir mes connaissances grâce à la pratique en entreprise.</p>
+      </div>
     </div>
   </div>
 </template>
@@ -98,8 +109,20 @@
 
 .page,
 .preview {
-  width: 40%;
+  width: 45%;
+}
+
+.page .text,
+.preview .text {
+  border-top: 2px solid rgba(255, 255, 255, 0.2);
   padding: 40px;
+}
+
+.close {
+  justify-content: space-between;
+  width: 25%;
+  border-right: 2px solid rgba(255, 255, 255, 0.2);
+  padding: 10px;
 }
 
 .page {
@@ -107,7 +130,7 @@
   border-right: 2px solid rgba(255, 255, 255, 0.2);
 }
 
-.page p {
+.page .text p {
   padding-bottom: 15px;
 }
 
@@ -115,13 +138,16 @@ h1 {
   border-bottom: 2px solid rgba(255, 255, 255, 0.2);
 }
 
-.preview p {
+.close {
+  display: flex;
+}
+
+.preview .text p {
   padding-top: 15px;
 }
 
-.preview {
-  width: 45%;
-  color: hsla(0, 0%, 100%, 0.8);
+.preview .text {
+  color: rgba(255, 255, 255, 0.8);
 }
 
 .personal {
