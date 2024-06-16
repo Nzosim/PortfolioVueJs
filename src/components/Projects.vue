87e1d909b20d0d02bf12119d8187c53646ca3277
nzosim <nzosim54@gmail.com>
2024-06-16 18:01:56 +0200

    project list

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,15 +1,149 @@
 <template>
-  <div>My Projects comming soon</div>
-  <div>
-    <a href="https://github.com/Nzosim" class="text-red-lighten-2">https://github.com/Nzosim</a>
+  <div class="projects">
+    <div class="menu">
+      <div class="d-flex menu-all pt-3 pb-2 pl-2" @click="isAllOpen = !isAllOpen">
+        <v-icon>{{ isAllOpen ? 'mdi-menu-down' : 'mdi-menu-right' }}</v-icon>
+        <p>all-projects</p>
+      </div>
+      <div v-if="isAllOpen" class="personal">
+        <v-checkbox
+          :class="{ 'text-white': selected === true }"
+          class="pl-1"
+          v-model="selected"
+          label="all"
+        ></v-checkbox>
+      </div>
+      <div class="filter">
+        <div class="d-flex menu-all pb-2 pl-2" @click="isFilterOpen = !isFilterOpen">
+          <v-icon>{{ isFilterOpen ? 'mdi-menu-down' : 'mdi-menu-right' }}</v-icon>
+          <p>filter</p>
+        </div>
+        <div
+          v-if="isFilterOpen"
+          class="d-flex mt-1"
+          :class="{ 'text-white': pageName === 'filter' }"
+          @click="pageName = 'filter'"
+        >
+          <v-radio-group v-model="selected" class="radio text-grey-darken-1">
+            <v-radio
+              :class="{ 'text-white': selected === 'vue' }"
+              label="Vue.js"
+              value="vue"
+            ></v-radio>
+            <v-radio
+              :class="{ 'text-white': selected === 'js' }"
+              label="JavaScript"
+              value="js"
+            ></v-radio>
+            <v-radio
+              :class="{ 'text-white': selected === 'tcj' }"
+              label="HTML/CSS/JS"
+              value="tcj"
+            ></v-radio>
+            <v-radio
+              :class="{ 'text-white': selected === 'nuxt' }"
+              label="Nuxt.js"
+              value="nuxt"
+            ></v-radio>
+            <v-radio
+              :class="{ 'text-white': selected === 'java' }"
+              label="Java"
+              value="java"
+            ></v-radio>
+            <v-radio
+              :class="{ 'text-white': selected === 'php' }"
+              label="PHP"
+              value="php"
+            ></v-radio>
+            <v-radio
+              :class="{ 'text-white': selected === 'flutter' }"
+              label="Flutter"
+              value="flutter"
+            ></v-radio>
+          </v-radio-group>
+        </div>
+      </div>
+    </div>
+    <div class="content">
+      <projectsList />
+    </div>
   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+import projectsList from '@/components/projects-pages/projects-list.vue'
+
+const isAllOpen = ref(true)
+const isFilterOpen = ref(true)
+const selected = ref('')
+</script>
+
 <style scoped>
-div {
-  font-size: 2em;
+.projects {
+  display: flex;
+  color: rgba(255, 255, 255, 0.4);
+}
+
+.menu {
+  width: 11%;
+}
+
+.content {
+  width: 89%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  height: calc(80vh - 20px);
+  padding-left: calc(20px);
+  padding-right: auto;
+}
+
+.menu-all {
+  border-bottom: 1px solid rgba(255, 255, 255, 0.05);
   color: white;
-  text-align: center;
-  margin-top: 200px;
+}
+
+.content {
+  border-left: 1px solid rgba(255, 255, 255, 0.05);
+}
+
+.menu-all:hover {
+  background: #363535;
+  transition: 0.1s;
+}
+
+@media screen and (max-width: 1840px) {
+  .menu {
+    width: 20%;
+  }
+}
+
+@media screen and (max-width: 1625px) {
+  /* .content {
+    padding-left: 130px;
+  } */
+}
+
+@media screen and (max-width: 930px) {
+  .menu {
+    width: 30%;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .projects {
+    flex-direction: column;
+  }
+  .menu {
+    width: 100%;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.05);
+    padding-bottom: 10px;
+  }
+  .content {
+    width: 100%;
+    height: calc(80vh);
+  }
 }
 </style>
+
+-
---
+++ src/components/projects-pages/projects-list.vue
@@ -0,0 +1,142 @@
+<template>
+  <div v-for="item in projects" :key="item" class="all">
+    <p class="pb-3 text-red-lighten-2">
+      {{ item.title }}
+    </p>
+    <div class="card">
+      <img :src="item.image" alt="project1" width="100%" />
+      <div class="card_content">
+        <p>{{ item.description }}</p>
+        <v-btn class="btn" href="/">view details</v-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import alClateImage from '@/assets/alclate.png'
+import forumImage from '@/assets/forum.png'
+import musicBrainzImage from '@/assets/musicbrainz.png'
+import pizzaShop from '@/assets/pizzashop.png'
+import photo from '@/assets/photo.png'
+import thisImage from '@/assets/this.png'
+import miniPress from '@/assets/minipress.png'
+import cIcone from '@/assets/cIcone.png'
+import netvod from '@/assets/netvod.png'
+import discord from '@/assets/discord.jpg'
+
+let projects = [
+  {
+    title: 'AlClate',
+    description:
+      'AlClate is a small web project with an HTML/CSS/JSS frontend and a PHP backend which allows you to notify the late arrivals of Edouard',
+    image: alClateImage,
+    type: ['js', 'hcj', 'php']
+  },
+  {
+    title: 'Nuxt forum',
+    description:
+      'Small forum created for my studies with Nuxt.jso, it uses websockets to update for everyone after adding a post',
+    image: forumImage
+  },
+  {
+    title: 'Music Brainz',
+    description:
+      'Description of this aaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa  aaaaaaaaaaaaa',
+    image: musicBrainzImage
+  },
+  {
+    title: 'PizzaShop',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: pizzaShop
+  },
+  {
+    title: 'Photo Website',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: photo
+  },
+  {
+    title: 'My Portfolio',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: thisImage
+  },
+  {
+    title: 'MiniPress',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: miniPress
+  },
+  {
+    title: 'GiftBox',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: pizzaShop
+  },
+  {
+    title: 'MBash',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: cIcone
+  },
+  {
+    title: 'NetVod',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: netvod
+  },
+  {
+    title: 'Discord bot',
+    description:
+      'Description of this sssssssssssssssssssss projecssssssss ssss ss ssssssssssssss sssssssssssssss ssssssss ssssssssssss',
+    image: discord
+  }
+]
+</script>
+
+<style scoped>
+.all {
+  margin: 20px;
+  width: 30%;
+  display: inline-block;
+}
+
+.card {
+  background-color: #181818;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+img {
+  height: 280px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+.card_content {
+  padding: 5px 20px;
+}
+
+.btn {
+  margin: 20px 0;
+  color: white;
+  background: #2c2c2c;
+  text-transform: lowercase;
+}
+
+@media screen and (max-width: 1625px) {
+  .all {
+    width: 40%;
+  }
+}
+
+@media screen and (max-width: 700px) {
+  .all {
+    width: 90%;
+  }
+}
+</style>
